500192d3530deece7b64e4d84536cc44a3a97769
cjgmj <cjgmj@hotmail.com>
2020-10-29 19:26:38 +0100

    Typescript

---
+++ jquery-avanzado/js/071.js
@@ -0,0 +1,6 @@
+var saludar = function (nombre) { return console.log("Hola " + nombre); };
+// const persona = "John";
+var persona = {
+    nombre: "John"
+};
+saludar(persona.nombre);
---
+++ jquery-avanzado/js/071.ts
@@ -0,0 +1,9 @@
+const saludar = (nombre: string) => console.log(`Hola ${nombre}`);
+
+// const persona = "John";
+
+const persona = {
+  nombre: "John",
+};
+
+saludar(persona.nombre);
